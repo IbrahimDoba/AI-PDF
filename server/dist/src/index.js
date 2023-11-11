c3e1490f2719cfa25b24b390791a7246cfcb7818
Ibrahim doba <ibrahimdoba55@gmail.com>
2023-11-11 11:37:40 +0100

    build fixes

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 require('dotenv').config();
 const mongoose = require("mongoose");
-const MyRoutes = require("../routes/routes.ts");
+const MyRoutes = require("../routes/routes.js");
 const cors = require("cors");
 //For env File
 const URI = "mongodb+srv://ibrahimdoba55:ibrahim123@authdb.kuauwfm.mongodb.net/AuthDB?retryWrites=true&w=majority";
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use('./files', express.static("files"));
 app.use("/", MyRoutes);
-const port = process.env.PORT || 5001;
+const port = 6001;
 app.listen(port, () => {
     console.log(`Server is Fire at http://localhost:${port}`);
 });
