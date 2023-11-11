e5cefdb52052a883a3cb7bed2e531d3f648ccb8d
Ibrahim doba <ibrahimdoba55@gmail.com>
2023-11-11 11:58:42 +0100

    build fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 require('dotenv').config()
 const mongoose = require("mongoose");
-const MyRoutes = require("../routes/routes.ts");
+const MyRoutes = require("../routes/routes");
 const cors = require("cors")
 //For env File
 
