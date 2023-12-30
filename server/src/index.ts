dd6d3147ce718d00699bb684af07fabf3632bb3c
Ibrahim doba <ibrahimdoba55@gmail.com>
2023-12-30 15:46:37 +0100

    cors attmept fix 3

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 // package.json   "module": "commonjs", before langchain
 
 const express = require("express");
-require('dotenv').config()
+require("dotenv").config();
 const mongoose = require("mongoose");
 const MyRoutes = require("../routes/routes");
-const cors = require("cors")
+const app = express();
+
+const cors = require("cors");
 //For env File
 
 const URI =
@@ -12,12 +14,16 @@ const URI =
 
 mongoose.connect(URI, {}).then(() => console.log("Mongo Connceted"));
 
-const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type",
+  })
+);
 // { origin : "https://ai-pdf-eight.vercel.app" }
 app.use("/", MyRoutes);
-
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
