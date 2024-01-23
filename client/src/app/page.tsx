0863a7381097336494143fe1592b414f89a3ae84
Ibrahim doba <ibrahimdoba55@gmail.com>
2024-01-23 06:43:27 +0100

    client fix 5

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,12 +2,17 @@ import { AiPdfProvider } from "@/context/AiPdfProvider";
 // import MainArea from "@/components/MainArea/MainArea";
 import Navbar from "@/components/Nav/Navbar";
 import HomePage from "@/pages/HomePage";
+import AiPdfForm from "@/components/MainArea/AiPdfForm";
+import AiPdfDetails from "@/components/MainArea/AiPdfDetails";
 
 export default function Home() {
   return (
     <AiPdfProvider>
-        <Navbar />
-        <HomePage />
+      <Navbar />
+      <div className="flex justify-start items-start h-[93.4vh] w-full bg-gray-400 max-lg:flex-col">
+        <AiPdfForm />
+        <AiPdfDetails />
+      </div>
     </AiPdfProvider>
   );
 }
